e75a54a9b170a01eb0e4f46ad7c36f6f8f11aabd
IsaiasJD117 <104098161+IsaiasJD117@users.noreply.github.com>
2022-11-16 17:25:35 -0500

    Corrections

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import Badge from "react-bootstrap/Badge";
-import {marked} from "marked";
+import {marked} from 'marked';
+import DOMPurify from "dompurify";
 
 export default class App extends React.Component{
 constructor(props) {
   super(props);
   this.state={
-    markdown: "",
+    markdown: 
+    `# Welcome to My Markdown Previewer!
+## freeCodeCamp Certificate Challenge!
+---
+This is **Bold** Text!
+---
+Challenge Link: [https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-markdown-previewer](https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-markdown-previewer)
+---
+This is how to write \`inline\` code!
+\`\`\`
+//Code Block!
+let randomName = "Random";
+const changeRandomName = (newName) => {
+  randomName = newName;
+};
+changeRandomName("Not Random");
+\`\`\`
+---
+- Bullet Points
+* Can Be Done
+- Three Ways
+---
+Blockquote Example:
+> This is a blockquote
+> Also part of a blockquote
+Not A blockquote
+---
+![Random Picture](https://source.unsplash.com/random)`,
   };
 }
 
-updateMarkdown(markdown) {
-  this.setState({ markdown });  
+createMarkdown(){
+  const rawMarkup= DOMPurify.sanitize(marked(this.state.markdown));
+  return { __html: rawMarkup };
 }
-
 
 render() {
     
@@ -25,18 +53,20 @@ render() {
       padding:"10px",
     };
     var outputStyle={
-      width: "400px",
-      height: "50vh",
-      backgroundColor: "#DCDCDC",
+      width: "auto",
+      height: "auto",
       marginLeft: "auto",
+      backgroundColor: "#DCDCDC",
       marginRight: "auto",
       padding:"10px",
     };
 
+    var BackgroundColor={backgroundColor: "lightBlue"}
+
     return(
-      <div className="App">
+      <div className="App" style={BackgroundColor}>
         <div className="container">
-          <div className="row mt-4">
+          <div className="row mt-6">
             <div className="col text-center">
               <h1>
                 {" "}
@@ -49,12 +79,12 @@ render() {
 
 
           <div className="row mt-4">
-            <div className="col-md-6">
+            <div className="col-md-4">
                 {" "}
                 <div className="col text-center">
                   <h4>
                     <Badge className="text-align-center" variant="secondary">
-                      Markdown Input
+                      Editor
                     </Badge>
                   </h4>
                 </div>
@@ -65,7 +95,11 @@ render() {
                     style={inputStyle} 
                     value={this.state.markdown} 
                     onChange={(e) => {
-                      this.updateMarkdown(e.target.value);
+                      let outputText =(e.target.value);
+
+                      this.setState({
+                        markdown: outputText
+                      });
                     }}
                   >
                   </textarea>
@@ -85,7 +119,7 @@ render() {
               <div
                 id="preview"
                 style={outputStyle}
-                dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}}
+                dangerouslySetInnerHTML={this.createMarkdown()}
                 >
               </div>
             </div>
